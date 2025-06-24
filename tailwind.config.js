d93609cfc085649ef12cf504b43bb8224767077a
Jaden-Thompson <jadenthompson048@gmail.com>
2025-06-24 07:44:38 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        rose: {
+          400: "#e3a6ac",
+          500: "#e9b4bc"
+        }
+      }
+    }
+  },
+  plugins: [],
+}
